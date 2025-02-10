9d88816bcddd3f56e731b43a4736400781fabf49
Pra-Tar <pranchal0112@gmail.com>
2025-02-10 04:35:25 +0000

    added auto slide for carousal

--- src/components/Carousal.jsx
+++ src/components/Carousal.jsx
@@ -1,67 +1,83 @@
 import dummyImage from "../assets/dummy.jpg";
-import dummyImage2 from "../assets/dummy2.jpg"
-import dummyImage3 from "../assets/dummy3.jpg"
-import dummyImage4 from "../assets/dummy4.jpg"
-import dummyImage5 from "../assets/dummy5.png"
-import dummyImage6 from "../assets/dummy6.png"
-import {Play,  ChevronLeft, ChevronRight } from 'lucide-react'
-import { useState } from "react";
+import dummyImage2 from "../assets/dummy2.jpg";
+import dummyImage3 from "../assets/dummy3.jpg";
+import dummyImage4 from "../assets/dummy4.jpg";
+import dummyImage5 from "../assets/dummy5.png";
+import dummyImage6 from "../assets/dummy6.png";
+import { Play, ChevronLeft, ChevronRight } from "lucide-react";
+import { useState, useEffect } from "react";
 
 const Carousal = () => {
-  const images = [dummyImage, dummyImage2, dummyImage3, dummyImage4, dummyImage5, dummyImage6];
+  const images = [
+    dummyImage,
+    dummyImage2,
+    dummyImage3,
+    dummyImage4,
+    dummyImage5,
+    dummyImage6,
+  ];
   const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prevCurrent) => (prevCurrent + 1) % images.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [images.length]);
 
   return (
     <>
       <div className="relative w-full h-[90vh] sm:h-[90vh] md:h-[70vh] lg:h-[80vh] overflow-hidden">
-        
-          {images.map((img, index) => (
-            <div key={index}
+        {images.map((img, index) => (
+          <div
+            key={index}
             className={`absolute inset-0 bg-cover bg-center flex items-center w-full h-[90vh] sm:h-[90vh] md:h-[70vh] lg:h-[80vh] px-4 transition-opacity duration-700 ${
-                index === current ? "opacity-100" : "opacity-0"
-              }`}
-              style={{ backgroundImage: `url(${img})` }}
-            >
-            <div className="bg-opacity-30 backdrop-blur-sm border border-white/30 lg:ml-6 text-white p-3 sm:p-2 md:p-3 lg:p-4 rounded-lg flex flex-col justify-between h-auto sm:h-1/2 w-full sm:w-3/4 md:w-2/3 lg:w-1/2">
+              index === current ? "opacity-100" : "opacity-0"
+            }`}
+            style={{ backgroundImage: `url(${img})` }}
+          >
+            <div className="bg-opacity-30 backdrop-blur-sm border border-white/30 lg:ml-6 text-white p-3 sm:p-2 md:p-3 lg:p-4 rounded-lg flex flex-col justify-between h-auto sm:h-1/2 w-full sm:w-3/4 md:w-2/3 lg:w-1/3">
               <div className="flex flex-wrap gap-2 text-xs font-extralight">
                 <span>Category</span>
                 <span>Year</span>
                 <span>Lang</span>
                 <span>Duration</span>
               </div>
-              <div className="text-2xl sm:text-4xl lg:text-3xl font-bold">TITLE</div>
-              <div className="text-md sm:text-xl lg:text-sm font-extralight">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius delectus magni ducimus soluta. Corporis saepe ab ipsam nam aspernatur, molestias ratione voluptatem quos placeat neque iusto, nobis ad. Quae, illum!
+              <div className="text-2xl sm:text-4xl lg:text-4xl font-bold">
+                TITLE
+              </div>
+              <div className="text-md sm:text-xl lg:text-xl font-extralight">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                delectus magni ducimus soluta. Corporis saepe ab ipsam nam
+                aspernatur, molestias ratione voluptatem quos placeat neque
+                iusto, nobis ad. Quae, illum!
               </div>
               <div className="flex text-sm flex-wrap gap-4 text-black">
                 <button className="border flex gap-x-1 items-center justify-center bg-white cursor-pointer rounded-2xl py-2 px-4 w-full sm:w-auto">
-                  <span>Watch Now</span> <Play size={18}/>
+                  <span>Watch Now</span> <Play size={18} />
                 </button>
                 <button className="border bg-white cursor-pointer rounded-2xl py-2 px-4 w-full sm:w-auto">
                   Add to Watchlist
                 </button>
               </div>
             </div>
-            </div>
-          ))}
+          </div>
+        ))}
       </div>
-
-      
 
       {/* DOTS */}
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-          {images.map((_, index) => (
-            <div
-              key={index}
-              className={`h-2 w-2 rounded-full ${
-                index === current ? "bg-white" : "bg-gray-400"
-              }`}
-              onClick={() => setCurrent(index)}
-            ></div>
-          ))}
-        </div>
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`h-2 w-2 rounded-full ${
+              index === current ? "bg-white" : "bg-gray-400"
+            }`}
+            onClick={() => setCurrent(index)}
+          ></div>
+        ))}
+      </div>
     </>
-
   );
 };
 
--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Menu, X, Heart, Search } from "lucide-react"; // library used for common icons.
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  });
 
   return (
     <header className="bg-opacity-30 backdrop-blur-lg border border-white/30 text-white shadow-md sticky top-0 z-50">
@@ -10,13 +18,16 @@ const Header = () => {
         <h1 className="text-xl font-bold">MOVIE</h1>
         <div className="relative flex items-center">
           <input
-            className="lg:w-80 md:w-50 sm:w-30 border rounded-full  pr-10 pl-4 h-8"
+            className="lg:w-80 md:w-50 sm:w-28 border rounded-full pr-10 pl-4 h-8"
             type="search"
             name="search"
             id="search"
             placeholder="Search..."
           />
-          <Search size={23} className="text-white absolute right-3 cursor-pointer" />
+          <Search
+            size={23}
+            className="text-white absolute right-3 cursor-pointer"
+          />
         </div>
 
         {/* Desktop Navigation */}
@@ -36,11 +47,14 @@ const Header = () => {
           >
             Watchlist
             <div className="pl-2">
-              <Heart size={18} color="white" className="fill-white group-hover:stroke-black" />
+              <Heart
+                size={18}
+                color="white"
+                className="fill-white group-hover:stroke-black"
+              />
             </div>
           </a>
         </nav>
-
 
         {/* Sign In & Register (Desktop) */}
         <nav className="hidden md:flex space-x-3">
@@ -60,7 +74,7 @@ const Header = () => {
 
         {/* Hamburger Menu Button */}
         <button
-          className="md:hidden focus:outline-none hover:cursor-pointer"
+          className="md:hidden focus:outline-none hover:cursor-pointer z-50"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
@@ -69,38 +83,42 @@ const Header = () => {
 
       {/* Mobile & Medium Devices Menu */}
       {isMenuOpen && (
-        <nav className="md:hidden bg-purple-400 flex flex-col space-y-2 p-4">
-          <a href="#" className="block py-2 hover:text-gray-200">
-            Home
-          </a>
-          <a href="#" className="block py-2 hover:text-gray-200">
-            Movies
-          </a>
-          <a href="#" className="block py-2 hover:text-gray-200">
-            Series
-          </a>
-          <a href="#" className="block py-2 hover:text-gray-200">
-            Kids
-          </a>
-          <a
-            href="#"
-            className="group flex items-center rounded-full pr-2"
-          >
-            Watchlist
-            <div className="pl-2">
-              <Heart size={18} color="white" className="fill-white group-hover:stroke-purple-400" />
-            </div>
-          </a>
-          <div className="md:hidden flex justify-between space-x-4 p-4">
+        <nav className="md:hidden bg-black flex flex-col space-y-2 pt-6 fixed inset-0 z-40 h-screen top-0 overflow:hidden">
+          <div className="pl-8 flex-row space-between text-xl">
+            <a href="#" className="block py-2 hover:text-gray-200">
+              Home
+            </a>
+            <a href="#" className="block py-2 hover:text-gray-200">
+              Movies
+            </a>
+            <a href="#" className="block py-2 hover:text-gray-200">
+              Series
+            </a>
+            <a href="#" className="block py-2 hover:text-gray-200">
+              Kids
+            </a>
+            <a href="#" className="group flex items-center rounded-full pr-2">
+              Watchlist
+              <div className="pl-2">
+                <Heart
+                  size={18}
+                  color="white"
+                  className="fill-white group-hover:stroke-black"
+                />
+              </div>
+            </a>
+          </div>
+
+          <div className="md:hidden flex justify-between space-x-4 p-4 text-md">
             <a
               href="#"
-              className="hover:text-purple-400 hover:bg-white shadow-2xl border rounded-full pr-2 pl-2 w-full text-center"
+              className="hover:text-black hover:bg-white shadow-2xl border rounded-full pr-2 pl-2 w-full text-center"
             >
               SignIn
             </a>
             <a
               href="#"
-              className="text-purple-400 bg-white hover:text-white hover:bg-purple-400 shadow-2xl border rounded-full pr-2 pl-2 w-full text-center"
+              className="text-black bg-white hover:text-white hover:bg-black shadow-2xl border rounded-full pr-2 pl-2 w-full text-center"
             >
               Register
             </a>
