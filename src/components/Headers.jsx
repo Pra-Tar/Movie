6e1ce98396118fc62b75146d60e2367ee297bcda
Pra-Tar <pranchal0112@gmail.com>
2025-02-12 21:54:12 +0530

    hide navbar and search before user is logged in.

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { Menu, X, Heart, Search } from "lucide-react"; // library used for common icons.
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation(); 
 
   useEffect(() => {
     if (isMenuOpen) {
@@ -14,27 +15,33 @@ const Header = () => {
     }
   });
 
+  const hideSearchBar = location.pathname === "/signin" || location.pathname === "/register" || location.pathname === "/";
+  const hideNavbar = location.pathname === "/signin" || location.pathname === "/register" || location.pathname === "/";
+
   return (
     <header className="bg-opacity-30 backdrop-blur-lg border border-white/30 text-white shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
         <Link to="/" className="text-xl font-bold">
           MOVIE
         </Link>
-        <div className="relative flex items-center">
-          <input
-            className="lg:w-80 md:w-50 sm:w-28 border rounded-full pr-10 pl-4 h-8"
-            type="search"
-            name="search"
-            id="search"
-            placeholder="Search..."
-          />
-          <Search
-            size={23}
-            className="text-white absolute right-3 cursor-pointer"
-          />
-        </div>
+        {!hideSearchBar && (
+          <div className="relative flex items-center">
+            <input
+              className="lg:w-80 md:w-50 sm:w-28 border rounded-full pr-10 pl-4 h-8"
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Search..."
+            />
+            <Search
+              size={23}
+              className="text-white absolute right-3 cursor-pointer"
+            />
+          </div>
+        )}
 
         {/* Desktop Navigation */}
+        { !hideNavbar && (
         <nav className="hidden md:flex lg:space-x-12 md:space-x-8">
           <Link
             to="/movies"
@@ -68,6 +75,7 @@ const Header = () => {
             </div>
           </Link>
         </nav>
+        )}
 
         {/* Sign In & Register (Desktop) */}
         <nav className="hidden md:flex space-x-3">
@@ -98,21 +106,22 @@ const Header = () => {
       {isMenuOpen && (
         <nav className="md:hidden bg-black flex flex-col space-y-2 pt-6 fixed inset-0 z-40 h-screen top-0 overflow:hidden">
           <div className="pl-8 flex-row space-between text-xl">
-            <Link to="/" className="block py-2 hover:text-gray-200">
+            <Link to="/" className="block py-2 hover:text-gray-200" onClick={() => setIsMenuOpen(false)}>
               Home
             </Link>
-            <Link to="/movies" className="block py-2 hover:text-gray-200">
+            <Link to="/movies" className="block py-2 hover:text-gray-200" onClick={() => setIsMenuOpen(false)}>
               Movies
             </Link>
-            <Link to="/series" className="block py-2 hover:text-gray-200">
+            <Link to="/series" className="block py-2 hover:text-gray-200" onClick={() => setIsMenuOpen(false)}>
               Series
             </Link>
-            <Link to="/kids" className="block py-2 hover:text-gray-200">
+            <Link to="/kids" className="block py-2 hover:text-gray-200" onClick={() => setIsMenuOpen(false)}>
               Kids
             </Link>
             <Link
               to="/watchlist"
               className="group flex items-center rounded-full pr-2"
+              onClick={() => setIsMenuOpen(false)}
             >
               Watchlist
               <div className="pl-2">
@@ -129,12 +138,14 @@ const Header = () => {
             <Link
               to="/signin"
               className="hover:text-black hover:bg-white shadow-2xl border rounded-full pr-2 pl-2 w-full text-center"
+              onClick={() => setIsMenuOpen(false)}
             >
               SignIn
             </Link>
             <Link
               to="/register"
               className="text-black bg-white hover:text-white hover:bg-black shadow-2xl border rounded-full pr-2 pl-2 w-full text-center"
+              onClick={() => setIsMenuOpen(false)}
             >
               Register
             </Link>
